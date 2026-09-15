4ca209d6b563a4454b0f79cfaba4e58426421dbb
FiorellaMercado <mercadofiorella598@gmail.com>
2026-09-15 00:10:29 -0300

    feat: add Express server setup following MVC structure and temas routes

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
+const express=require('express');
+const app= express();
+const router = require('./routes/temasRoutes');
+const PORT = 3000
 
+app.set('view engine','ejs');
+app.use('/temas', router);
+
+app.listen(PORT, () => {
+    console.log(`Corriendo en http://localhost:${PORT}` )
+} );
 
 
 
