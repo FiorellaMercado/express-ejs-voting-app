e50d990c06f8f9789d62545d029c15c07d7fa9f5
Fiorella Mercado <mercadofiorella598@gmail.com>
2026-09-17 06:12:33 -0300

    add database.js

---
+++ database.js
@@ -0,0 +1,22 @@
+const Database = require('better-sqlite3');
+const db = new Database('database.db');
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS temas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre TEXT NOT NULL,
+    votos INTEGER DEFAULT 0
+  )
+`);
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS enlaces (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tema_id INTEGER NOT NULL,
+    enlace TEXT NOT NULL,
+    votos INTEGER DEFAULT 0,
+    FOREIGN KEY (tema_id) REFERENCES temas(id)
+  )
+`);
+
+module.exports = db;
