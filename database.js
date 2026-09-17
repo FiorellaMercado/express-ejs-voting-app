e523137d023914230725225f1b6b8d861f9bc64a
FiorellaMercado <mercadofiorella598@gmail.com>
2026-09-17 09:21:46 -0300

    feat: replace arrays with SQLite DB

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const Database = require('better-sqlite3');
-const db = new Database('database.db');
+const db = new Database('temas.db');
 
 db.exec(`
   CREATE TABLE IF NOT EXISTS temas (
--- models/tema.js
+++ models/tema.js
@@ -1,84 +1,88 @@
-const temas = []
-
-//CRUD temas
+const db = require('../database');
 
 //read
 const getTemas = () => {
-    return temas
-}
+    const temas = db.prepare('SELECT * FROM temas ORDER BY votos DESC').all();
+    //para ordenar tambien los enlaces de cada tema
+    temas.forEach(tema => {
+        tema.enlaces = db.prepare('SELECT * FROM enlaces WHERE tema_id = ? ORDER BY votos DESC').all(tema.id);
+    });
 
-const getTemaId=(id)=> {
-    const tema_encontrado=temas.find(tema => tema.id === parseInt(id) )
-    return tema_encontrado
-}
+    return temas;
+};
+
+const getTemaId = (id) => {
+    const tema = db.prepare('SELECT * FROM temas WHERE id = ?').get(id);
+    return tema;
+};
 
 //create
-const crearTema=(nombre)=> {
-    //id incremental
-    const pseudo_id=temas.length+1;
-    const tema={id:pseudo_id, nombre: nombre, votos:0, enlaces:[]}
-    temas.push(tema)
-}
+const crearTema = (nombre) => {
+    const query = db.prepare('INSERT INTO temas (nombre) VALUES (?)');
+    query.run(nombre);
+};
 
-//delte
-const deleteTemaId = (id)=> {
-    const indice=temas.findIndex(tema => tema.id === parseInt(id))
-    temas.splice(indice, 1)
-}
+//delete
+const deleteTemaId = (id) => {
+    // primero los enlaces del tema , efecto cascada
+    const borrarEnlaces = db.prepare('DELETE FROM enlaces WHERE tema_id = ?');
+    borrarEnlaces.run(id);
+
+    const borrarTema = db.prepare('DELETE FROM temas WHERE id = ?');
+    borrarTema.run(id);
+};
 
 //update
-const updateTema=(nombre, id)=>{
-    const indice= temas.findIndex(tema => tema.id === parseInt(id))
-    temas[indice].nombre=nombre
-}
+const updateTema = (nombre, id) => {
+    const query = db.prepare('UPDATE temas SET nombre = ? WHERE id = ?');
+    query.run(nombre, id);
+};
 
 // CRUD enlaces
+
+//read
 const getEnlaces = (idTema) => {
-    const tema_encontrado=temas.find(tema=>tema.id === parseInt(idTema))
-    const enlaces= tema_encontrado.enlaces
-    return enlaces
-}
-
-const createEnlace = (idTema,nuevo_enlace) => {
-    const enlaces= getEnlaces(idTema)
-    const pseudo_id= enlaces.length+1
-    const enlace={id: pseudo_id, enlace: nuevo_enlace, votos: 0}
-    return enlaces.push(enlace)
-}
-const deleteEnlace = (idTema, idEnlace)=>{
-    const enlaces = getEnlaces(idTema)
-    const indice = enlaces.findIndex(enlace=> enlace.id===parseInt(idEnlace))
-    enlaces.splice(indice,1)
-}
-
-// enlace aqui es un string que viene del input
-const updateEnlace = (idTema, idEnlace,str_enlace) => {
-    const enlace=getEnlaceId(idTema,idEnlace)
-    enlace.enlace=str_enlace
-    
-}
+    const enlaces = db.prepare('SELECT * FROM enlaces WHERE tema_id = ? ORDER BY votos DESC').all(idTema);
+    return enlaces;
+};
 
 const getEnlaceId = (idTema, idEnlace) => {
-    const enlaces=getEnlaces(idTema)
-    const indice=enlaces.findIndex(enlace => enlace.id === parseInt(idEnlace))
-    return enlaces[indice]
+    const enlace = db.prepare('SELECT * FROM enlaces WHERE id = ? AND tema_id = ?').get(idEnlace, idTema);
+    return enlace;
+};
 
-}
+//create
+const createEnlace = (idTema, nuevo_enlace) => {
+    const query = db.prepare('INSERT INTO enlaces (tema_id, enlace) VALUES (?, ?)');
+    query.run(idTema, nuevo_enlace);
+};
+
+const deleteEnlace = (idTema, idEnlace) => {
+    const query = db.prepare('DELETE FROM enlaces WHERE id = ? AND tema_id = ?');
+    query.run(idEnlace, idTema);
+};
+
+// enlace aquí es un string que viene del input
+const updateEnlace = (idTema, idEnlace, str_enlace) => {
+    const query = db.prepare('UPDATE enlaces SET enlace = ? WHERE id = ? AND tema_id = ?');
+    query.run(str_enlace, idEnlace, idTema);
+};
 
 // votos
 
-const votarTema = (idTema)=>{
-    const tema= getTemaId(idTema)
-    tema.votos= tema.votos+1
-}
-const votarEnlace=(idTema,idEnlace)=>{
-    const enlace=getEnlaceId(idTema,idEnlace)
-    enlace.votos=enlace.votos+1
+const votarTema = (idTema) => {
+    const query = db.prepare('UPDATE temas SET votos = votos + 1 WHERE id = ?');
+    query.run(idTema);
+};
 
-}
+const votarEnlace = (idTema, idEnlace) => {
+    const query = db.prepare('UPDATE enlaces SET votos = votos + 1 WHERE id = ? AND tema_id = ?');
+    query.run(idEnlace, idTema);
+};
 
 // exportar lo que voy a usar en controller
-module.exports={getTemas,getTemaId,crearTema, deleteTemaId, updateTema,
+module.exports = {
+    getTemas, getTemaId, crearTema, deleteTemaId, updateTema,
     getEnlaces, getEnlaceId, createEnlace, deleteEnlace, updateEnlace,
     votarTema, votarEnlace
-}
+};
