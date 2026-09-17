a91635137ac6c4c08a8d88c05a2e8e7125821e94
FiorellaMercado <mercadofiorella598@gmail.com>
2026-09-16 21:12:52 -0300

    feat: add JS logic to sort topics and links by vote count

--- public/js/votos.js
+++ public/js/votos.js
@@ -7,21 +7,47 @@ document.querySelectorAll('.btn-votar-tema').forEach(btn => {
             .then(res => res.json())
             .then(data => {
                 document.getElementById(`votos-tema-${idTema}`).textContent = data.votos;
+
+                const li = document.querySelector(`.li-temas[data-tema-id="${idTema}"]`);
+                li.setAttribute('data-votos', data.votos);
+
+                ordenarTemas();
             });
     });
 });
 
-
+// votar enlaces
 document.querySelectorAll('.btn-votar-enlace').forEach(btn =>{
     btn.addEventListener('click',()=>{
         const idTema = btn.dataset.temaId;
         const idEnlace = btn.dataset.enlaceId;
-        console.log(idEnlace)
         fetch(`/temas/votarEnlace/tema/${idTema}/enlace/${idEnlace}`, {method:'POST'})
             .then(res => res.json())
             .then(data => {
-                console.log(data.votos)
-                document.getElementById(`votos-enlace-${idEnlace}`).textContent = data.votos;
+                document.querySelector(`.lista-enlace[data-tema-id="${idTema}"] #votos-enlace-${idEnlace}`).textContent = data.votos;
+
+                const li = document.querySelector(`.lista-enlace[data-tema-id="${idTema}"] .li-enlace[data-enlace-id="${idEnlace}"]`);
+                li.setAttribute('data-votos', data.votos);
+
+                ordenarEnlaces(idTema);
             });
     });
 });
+
+// ordenar temas
+function ordenarTemas(){
+    const padre = document.querySelector('.lista-tema');
+    const lista = Array.from(padre.children);
+
+    lista.sort((a, b) => b.dataset.votos - a.dataset.votos);
+    lista.forEach(li => padre.appendChild(li));
+}
+
+// ordenar enlaces
+function ordenarEnlaces(idTema){
+    const padre = document.querySelector(`.lista-enlace[data-tema-id="${idTema}"]`);
+    const lista = Array.from(padre.children);
+
+    lista.sort((a,b)=> b.dataset.votos - a.dataset.votos);
+    lista.forEach(li => padre.appendChild(li));
+}
